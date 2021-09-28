30b658034a58889e3dbccdf5aff2151c5e8fac52
wato-mag <mag01859@mitsuiwa.co.jp>
2021-09-28 21:49:50 +0900

    first commit

---
+++ pages/blog/[id].js
@@ -0,0 +1,38 @@
+import { client } from "../../libs/client";
+import styles from '../../styles/Home.module.scss';
+
+export async function getStaticProps(context){
+  console.log(context)
+  const id = context.params.id
+  const data = await client.get({ endpoint: 'blog', contentId: id })
+  return {
+    props: {
+      blog: data
+    }
+  }
+}
+
+export async function getStaticPaths(){
+  const data = await client.get({ endpoint: "blog"})
+  console.log(data)
+
+  const paths = data.contents.map((content) => {
+    return {params: { id: `${content.id}` } }
+  })
+  console.log(paths)
+  return {paths: paths, fallback: false}
+}
+
+
+
+export default function BlogId({ blog }){
+  console.log(blog)
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <div className={styles.post} dangerouslySetInnerHTML={{__html: `${blog.body}`,}}
+      />
+    </main>
+  )
+}
